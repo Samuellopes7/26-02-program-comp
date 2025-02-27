e6fef44c2041d0858035c3ea3cef6a29cfbdf817
Samuel Lopes <samuel.lopes.a007@gmail.com>
2025-02-26 21:40:17 -0300

    Add files via upload

---
+++ aula dia 26/main.py
@@ -0,0 +1,7 @@
+
+numero = 250
+
+if numero % 3 == 0 and numero % 5 == 0:
+    print(f"O número {numero} é múltiplo de 3 e 5 ao mesmo tempo.")
+else:
+    print(f"O número {numero} não é múltiplo de 3 e 5 ao mesmo tempo.")
